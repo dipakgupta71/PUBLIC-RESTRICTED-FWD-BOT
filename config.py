81536ff5dde2d3ba2d71fe3bd121e2184551ac7c
dipakgupta71 <97516714+dipakgupta71@users.noreply.github.com>
2025-08-07 13:16:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ from os import environ
 class Config:
     API_ID = int(environ.get("API_ID", "5128432"))
     API_HASH = environ.get("API_HASH", "5436f836f2887d3e680628ff1010e949")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7600284293:AAGbkaU8cDNsRS6MAuPotgRCjj59jUix2Vw") 
     BOT_SESSION = environ.get("BOT_SESSION", "DOXDKGBOT") 
     DATABASE_URI = environ.get("DATABASE_URI", "")
     DATABASE_NAME = environ.get("DATABASE_NAME", "DOXDKGBOT")
@@ -27,3 +27,4 @@ class temp(object):
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
